780585ab6e6ea7b52745469432578c903cdea25f
barisyild <fb_bcy@hotmail.com>
2026-03-03 16:55:19 +0300

    Add PublicApiHandler and configure route for API domain handling

---
+++ src/hx/well/handler/PublicApiHandler.hx
@@ -0,0 +1,6 @@
+package hx.well.handler;
+class PublicApiHandler extends PublicHandler {
+    public override function publicPath():String {
+        return "public_api";
+    }
+}
--- src/hx/well/provider/BootProvider.hx
+++ src/hx/well/provider/BootProvider.hx
@@ -1,8 +1,12 @@
 package hx.well.provider;
 import hx.well.route.Route;
 import hx.well.handler.AbortHandler;
+import hx.well.handler.PublicApiHandler;
 class BootProvider extends AbstractProvider {
     public function boot():Void {
+        Route.domain("api.hxwell.org")
+            .handler(new PublicApiHandler());
+
         Route.get("/abort/{code}")
             .handler(new AbortHandler())
             .where("code", "\\b[1-5][0-9]{2}\\b");
